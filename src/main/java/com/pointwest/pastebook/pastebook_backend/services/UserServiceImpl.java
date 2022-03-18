cdee342a58ead2677a100acf1a92902b052ad6a1
Alexis Kyle Sales <alexis.sales@pointwest.com.ph>
2022-03-18 23:47:18 +0800

    Verification

--- src/main/java/com/pointwest/pastebook/pastebook_backend/services/UserServiceImpl.java
+++ src/main/java/com/pointwest/pastebook/pastebook_backend/services/UserServiceImpl.java
@@ -9,9 +9,14 @@ import com.pointwest.pastebook.pastebook_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
@@ -27,6 +32,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private JwtToken jwtToken;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+
     @Override
     public User createUser(User user) {
         Optional<User> userDb = Optional.ofNullable(userRepository.findByEmail(user.getEmail()));
@@ -35,7 +44,13 @@ public class UserServiceImpl implements UserService {
         }
         //When verified, change status to verify and set profileUrl
         userRepository.save(user);
-
+        try {
+            sendVerificationEmail(user, "http://localhost:4200");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         return userRepository.save(prodVerify(user));
     }
 
@@ -78,7 +93,52 @@ public class UserServiceImpl implements UserService {
         userDb.setProfileUrl(profileUrl);
         return userRepository.save(userDb);
     }
+//
+//    public boolean verify(String verificationCode) {
+//        User user = userRepository.findByVerificationCode(verificationCode);
+//
+//        if (user == null || user.isEnabled()) {
+//            return false;
+//        } else {
+//            user.setVerificationCode(null);
+//            user.setEnabled(true);
+//            userRepository.save(user);
+//
+//            return true;
+//        }
+//
+//    }
+    // send verification email
+    private void sendVerificationEmail(User user, String siteURL)
+            throws MessagingException, UnsupportedEncodingException {
+        String toAddress = user.getEmail();
+        String fromAddress = "grp4pastebook@gmail.com";
+        String senderName = "Group 4 Pastebook";
+        String subject = "Please verify your registration";
+        String content = "Dear [[name]],<br>"
+                + "Please click the link below to verify your registration:<br>"
+                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
+                + "Thank you,<br>"
+                + "Your company name.";
 
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(fromAddress, senderName);
+        helper.setTo(toAddress);
+        helper.setSubject(subject);
+
+        content = content.replace("[[name]]", user.getFirstName());
+        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
+
+        content = content.replace("[[URL]]", verifyURL);
+
+        helper.setText(content, true);
+
+        mailSender.send(message);
+
+        System.out.println("Email has been sent");
+    }
     // get users
     public ResponseEntity getUsers() {
         return new ResponseEntity(userRepository.findAll(), HttpStatus.OK);
